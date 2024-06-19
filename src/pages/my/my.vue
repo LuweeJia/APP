36879a930bf4ad63caa011501ccb37278ddf5a85
Luweijia110 <1109282554@qq.com>
2024-06-20 02:27:03 +0800

    测试用

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -7,16 +7,11 @@ const memberStore = useMemberStore()
 <template>
   <view class="my">
     <view>会员信息：{{ memberStore.profile }}</view>
-    <button
-      @tap="
-        memberStore.setProfile({
-          nickname: '黑马先锋',
-        })
-      "
-      size="mini"
-      plain
-      type="primary"
-    >
+    <button @tap="
+      memberStore.setProfile({
+        nickname: '黑马先锋1',
+      })
+      " size="mini" plain type="primary">
       保存用户信息
     </button>
     <button @tap="memberStore.clearProfile()" size="mini" plain type="warn">清理用户信息</button>
